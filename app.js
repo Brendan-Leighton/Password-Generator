23af9dcd5ce9806612d1820b89380c1fc685ccc6
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-01 02:12:05 -0400

    Password Parameters successfully captured into vars.

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+let useUpper = false;
+let useLower = false;
+let useNumber = false;
+let useSpecial = false;
+let passwordLength = 8;
+let password = "";
 
+function checkPasswordParameters() {
+    useUpper = document.getElementById("allow-uppercase").checked;
+    useLower = document.getElementById("allow-lowercase").checked;
+    useNumber = document.getElementById("allow-numbers").checked;
+    useSpecial = document.getElementById("allow-special-characters").checked;
+    passwordLength = document.getElementById("password-length").value;
+    // return useSpecial, useLower, useUpper, useNumber, passwordLength;
+}
 
+function generatePassword() {
+    checkPasswordParameters();
 
-const allowUpper = document.getElementById("allow-uppercase");
-const allowLower = document.getElementById("allow-lowercase");
-const allowNumber = document.getElementById("allow-numbers");
-const allowSpecial = document.getElementById("allow-special-characters");
+    test();
+}
+
+displayPassword = (password) => document.getElementById("password").innerHTML = password;
+
+function test() {
+    password = (console.log(`USE UPPER: ${useUpper}, USE LOWER: ${useLower}, USE NUMBERS: ${useNumber}, USE SPEC CHARS: ${useSpecial}, and LENGTH: ${passwordLength}.`))
+}
