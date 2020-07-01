2f1cea6d06f0563709daeebe894a1c1760206fbb
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-01 01:22:35 -0400

    Connected Generate button to JS. Set up variables for checkboxes.

---
+++ app.js
@@ -0,0 +1,4 @@
+const allowUpper = document.getElementById("allowUpper");
+const allowLower = document.getElementById("allowLower");
+const allowNumber = document.getElementById("allowNumber");
+const allowSpecial = document.getElementById("allowSpecial");
